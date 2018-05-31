c35e3fd26a3b05c005addcc93c9b6f5fff940b5a
Leon van Wissen <leon@vwissen.nl>
2018-05-31 15:44:53 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # collatieparser-docker
+###### Docker flask implementation around [lvanwissen/CollationParser](https://github.com/LvanWissen/CollationParser)
+
+Online [demo](https://leonvanwissen.nl/tools/collatieparser/).
+
+![Screenshot](static/collatieparser_screenshot2.png)
+
---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask import render_template, request
+
+from CollationParser.collationParser import CollationParser
+
+app = Flask(__name__)
+
+@app.route('/', methods= ['GET'])
+def serve():
+
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def process_data():
+    """
+    Process a collation formula from input in template. 
+    """
+
+    parser = CollationParser(verbose=True)
+    collateformula = request.form['data']
+
+    # result
+    folia, resultlist = parser.parse(collateformula, use_parselist=True)
+
+    resultlist = [{k:v for k,v in e.items() if v} for e in resultlist]
+
+    return render_template('index.html', result=resultlist, folia=folia, collatieformule=collateformula)
+
+@app.route('/result_ppn', methods=['POST'])
+def process_ppn():
+    """
+    Process a collation formula from a sparql request givenin the web interface by ppn. 
+    """
+
+    parser = CollationParser(verbose=True)
+    collateformula = fetch_ppn(request.form['ppn'])
+
+    # result
+    folia, resultlist = parser.parse(collateformula, use_parselist=True)
+
+    resultlist = [{k:v for k,v in e.items() if v} for e in resultlist]
+
+    return render_template('index.html', result=resultlist, folia=folia, collatieformule=collateformula)
+
+def fetch_ppn(ppn):
+    """
+
+    """
+
+    from SPARQLWrapper import SPARQLWrapper, JSON
+    ENDPOINT_URL = 'http://openvirtuoso.kbresearch.nl/sparql'
+
+    sparql = SPARQLWrapper(ENDPOINT_URL)
+
+    sqlquery = """
+
+    SELECT ?collatie WHERE {{
+
+        kbc:{ppn} dcterms:extent ?formaat, ?collatie .
+
+        FILTER (?formaat != ?collatie ) .
+        FILTER regex(?formaat, "^[0-9]{{1,2}}°", "i") .
+    }}
+    """.format(ppn=ppn)
+
+    sparql.setQuery(sqlquery)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    # {'head': {'link': [], 'vars': ['collatie']}, 'results': {'bindings': [{'collatie': {'value': '*`SUP`8`LO` A-S`SUP`8`LO` (S8 blank)', 'type': 'literal'}}], 'distinct': False, 'ordered': True}}
+
+    result = results['results']['bindings'][0]['collatie']['value']
+
+    return result
+
+if __name__ == '__main__':
+    # socketio.run(app, host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
+    #socketio.run(app)
+
